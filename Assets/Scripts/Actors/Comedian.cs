103c18f279a63e03bc468214c81fdb3dfafccf7c
Ralph Robes <ralphrobes123@gmail.com>
2024-01-27 11:06:56 -0500

    Setup for Comedian points for the string

--- Assets/Scripts/Actors/Comedian.cs
+++ Assets/Scripts/Actors/Comedian.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Comedian : MonoBehaviour
+public class Comedian : GameManager
 {
+    
+    private string[] comedianJokes = {
+        "!", "@", "#", "$", "%", "^", "&",
+        "*", "(", ")"
+    };
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    ///Joke score if message
+    enum JokeScore 
+    {
+        exclamtionPoint = 1, //! = 1
+        atPoint = 2, //@ = 2
+        hashTagPoint = 3, //# = 3
+        dolloarPoint = 4, // $ = 4 
+        precentPoint = 5, //% = 5
+        powerPoint = 6, // ^ = 6
+        andPoint  = 7, // & = 7
+        starPoint = 8, // * = 8
+        leftBracket = 9, // ( = 9
+        rightBracket = 0, // ) = 0
+    }
     // Start is called before the first frame update
     void Start()
     {
