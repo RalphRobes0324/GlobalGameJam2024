6a3d55a07b96ac1676be3211b7fef4970355c5d7
Ralph Robes <ralphrobes123@gmail.com>
2024-01-27 18:07:53 -0500

    Able to pass variable

--- Assets/Scripts/Actors/Audience.cs
+++ Assets/Scripts/Actors/Audience.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Audience : Comedian
+public class Audience : MonoBehaviour
 {
     private int scoreTheJoke;
     private int humourLevel;
+
+    GameObject comedian;
+    Comedian comedianScript;
     
     // Start is called before the first frame update
     private void Start()
     {
 
-        
-
+        comedian = GameObject.Find("obj_Comedian");
+        comedianScript = comedian.GetComponent<Comedian>();
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Debug.Log(comedianMakeJoke);
+        Debug.Log(comedianScript.comedianMakeJoke);
     }
 }
