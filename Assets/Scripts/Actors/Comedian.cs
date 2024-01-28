0b781ad45b33b1fef57b67769ac81aef3f1a4571
Ralph Robes <ralphrobes123@gmail.com>
2024-01-28 16:55:57 -0500

    made bgm loop

--- Assets/Scripts/Actors/Comedian.cs
+++ Assets/Scripts/Actors/Comedian.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 
 public class Comedian : GameManager
