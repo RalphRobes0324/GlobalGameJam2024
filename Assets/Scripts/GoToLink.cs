0496050fe5adc1b40882650c4dc73e10d55cf80f
Ralph Robes <ralphrobes123@gmail.com>
2024-01-28 13:44:11 -0500

    Maya and Ned buttons working

---
+++ Assets/Scripts/GoToLink.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class GoToLink : MonoBehaviour
+{
+    public Sprite normalState;
+    public Sprite hoverState;
+    public SpriteRenderer spriteRenderer;
+    private string userLink;
+
+    public string whoButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        userLink = "";
+
+        if(spriteRenderer != null && normalState != null)
+        {
+            spriteRenderer.sprite = normalState;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (whoButtons)
+        {
+            case "Maya":
+                userLink = "https://www.linkedin.com/in/maya-harry-9133b41a9";
+                break;
+            case "Ned":
+                userLink = "https://wakayrd.carrd.co/";
+                break;
+        }
+
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Application.OpenURL(userLink);
+    }
+    private void OnMouseOver()
+    {
+        if(spriteRenderer != null && hoverState != null)
+        {
+            spriteRenderer.sprite = hoverState;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (spriteRenderer != null && normalState != null)
+        {
+            spriteRenderer.sprite = normalState;
+        }
+    }
+}
