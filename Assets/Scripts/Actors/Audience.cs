e99a3d30a58b98b2cfb504589d08c99f631431a5
RalphRobes0324 <ralphrobes123@gmail.com>
2024-01-27 22:49:27 -0500

    Reset files

--- Assets/Scripts/Actors/Audience.cs
+++ Assets/Scripts/Actors/Audience.cs
@@ -4,41 +4,15 @@ using UnityEngine;
 
 public class Audience : MonoBehaviour
 {
-    private float scoreTheJoke;
-    private bool statusComedian;
-    public bool statusAudi;
-
-    [Header("Control Humour Level")]
-    public float humourLevel;
-
-    private GameObject comedian;
-    private Comedian comedianScript;
-    
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-
-        comedian = GameObject.Find("obj_Comedian");
-        comedianScript = comedian.GetComponent<Comedian>();
-        humourLevel = 1.5f;
-        statusAudi = false;
-
+        
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-
-        scoreTheJoke = comedianScript.jokeScore;
-        statusComedian = comedianScript.doneMakingJoke;
-        if(statusComedian && !statusAudi)
-        {
-            Debug.Log(scoreTheJoke);
-            statusAudi = true; //Ready listen to next joke
-            
-        }
-
-        
         
     }
 }
--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Comedian : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
