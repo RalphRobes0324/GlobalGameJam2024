a172cbab7b954b222209916fda0712f62835132b
Ralph Robes <ralphrobes123@gmail.com>
2024-01-28 15:12:52 -0500

    Button setup work

--- Assets/HelpScript.cs
+++ Assets/HelpScript.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HelpScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 }
